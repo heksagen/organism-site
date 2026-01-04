3d9cc428d149afa710e7c59f03358bf773daf91d
Gian Lyndon M. Cantere <canteregian38@gmail.com>
2026-01-04 15:06:58 +0800

    fix array to string conversion

---
+++ database/migrations/2026_01_04_070515_fix_taxonomy_column_on_species_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('species', function (Blueprint $table) {
+            $table->json('taxonomy')->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('species', function (Blueprint $table) {
+            $table->dropColumn('taxonomy');
+        });
+    }
+};
+
